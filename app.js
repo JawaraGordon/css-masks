9d9864bb969fc6ff149841605be4f5912d488cb1
JawaraGordon <jawaragordon@icloud.com>
2023-02-08 19:36:18 -0500

    added onchange function call when shape is chosen from the dropdown menu

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const addMask = (shape) => {
+const addMask = () => {
+  const shape = document.getElementById('shapes').value;
   const mask = document.querySelector('.mask');
   mask.classList.add(shape);
 };
 
-addMask('diamond');
+addMask();
