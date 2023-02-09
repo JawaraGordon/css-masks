472c91214e872c27d0a8a8ecef460afe4f4dfe23
JawaraGordon <jawaragordon@icloud.com>
2023-02-08 20:48:50 -0500

    debugged issue with mask class by adding a function to remove previous class onclick

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const addMask = () => {
   const shape = document.getElementById('shapes').value;
   const mask = document.querySelector('.mask');
   mask.classList.add(shape);
+  const shapeSelect = document.getElementById('shapes');
+  shapeSelect.addEventListener('click', function () {
+    mask.classList.remove(shape);
+  });
 };
 
 addMask();
